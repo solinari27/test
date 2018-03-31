42753350117098bdccee01ca22635be02b202b93
solinari27 <solinari27@gmail.com>
2018-04-01 06:46:41 +0800

    第一次爬虫.json
    Signed-off-by: solinari27 <solinari27@gmail.com>

--- stockcode/quoteCfi/quoteCfiSpider.py
+++ stockcode/quoteCfi/quoteCfiSpider.py
@@ -6,31 +6,12 @@
 @time: 2018/04/01 
 """  
 
-
-import scrapy
-import re
+import os
 
 class quoteCfiSpider(scrapy.Spider):
-    name = "quoteCfiSpider"
-    start_urls = ['http://quote.cfi.cn/stockList.aspx?t=11']
-    allowed_domains = ["quote.cfi.cn"]
 
-    def parse(self, response):
-        body = response.body
-        unicode_body = response.body_as_unicode ()
-        stocklist = {}
-        for sel in response.xpath ('//div[@id="divcontent"]'):
-            stocks = sel.re (r'<td><a href=*>*(.+?)</a>(.+?)</td>')
-            if not stocks:
-                pass
-            else:
-                for it in stocks:
-                    codestr = re.search(r'\d\d\d\d\d\d', it)
-                    code = codestr.group()
-                    left = re.search(r'(?<=\"\>).+?$', it)
-                    leftname = left.group()
-                    stocknamesplit = leftname.split("(")
-                    stockname = stocknamesplit[0]
-                    print code, stockname
-                    stocklist[code] = stockname
+    def __init__(self):
+
+    def crawl(self):
+        os.system("scrapy runspider stockcode/spider/scrapyCfiSpider.py -o Data/out.json --logfile Log/quoteCfiSpider.log --loglevel ERROR")
 
