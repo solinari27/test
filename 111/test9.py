b3fe9e9acf1ed998683c21cbfd9b671139018875
solinari <solinari27@gmail.com>
2018-06-13 19:18:52 +0800

    xxx

--- 111/test9.py
+++ 111/test9.py
@@ -16,10 +16,11 @@ def requestJson(url):
     # print content
 
 # sohu
-url = "http://q.stock.sohu.com/hisHq?code=cn_300228&start=20130930&end=20131231&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp"
-requestJson(url)
+# url = "http://q.stock.sohu.com/hisHq?code=cn_300228&start=20130930&end=20131231&stat=1&order=D&period=d&callback=historySearchHandler&rt=jsonp"
+# requestJson(url)
 
 # netease
 url = "http://quotes.money.163.com/service/chddata.html?code=0601857&start=20071105&end=20150618&fields=TCLOSE;HIGH;LOW;TOPEN;LCLOSE;CHG;PCHG;TURNOVER;VOTURNOVER;VATURNOVER;TCAP;MCAP"
+url = "http://quotes.money.163.com/service/chddata.html?code=0601857&start=20071105&end=20150618"
 requestJson(url)
 
