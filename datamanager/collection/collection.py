65a2e0a4519e8292dbdccabec377a6117fc30845
solinari <solinari27@gmail.com>
2019-02-07 11:10:00 +0800

    clear some code

--- datamanager/collection/collection.py
+++ datamanager/collection/collection.py
@@ -7,7 +7,6 @@
 """
 from common.mongo.sohuConn import SohuConn
 from common.mongo.neteaseConn import NeteaseConn
-from utils.common.switch import switch
 import time
 
 class Collection(object):
@@ -40,17 +39,6 @@ class Collection(object):
         for index, sohu_item in enumerate(sohu_result):
             netease_item = netease_result[index]
             item = {}
-            # for key in sohu_item.keys():
-            #     item[key] = sohu_item[key]
-            # for key in netease_item.keys():
-            #     if not key in sohu_item.keys():
-            #         item[key] = netease_item[key]
-            #         print key, "netease: ", netease_item[key]
-            #     else:
-            #         # assert (sohu_item[key] == netease_item[key])
-            #         print key, "netease: ", netease_item[key], "soho: ", sohu_item[key]
-            #         # find problem
-            #         pass
 
             for key in netease_item.keys():
                 for case in switch(key):
@@ -111,28 +99,3 @@ class Collection(object):
                     # No need to break here, it'll stop anyway
             result.append(item)
         return result
-
-
-# sohuconn = SohuConn("/home/solinari/workspace/stockCrawler/Conf/sohu.conf")
-# neteaseconn = NeteaseConn("/home/solinari/workspace/stockCrawler/Conf/netease.conf")
-#
-# s = time.localtime(time.time())
-# year = s.tm_year
-# mon = s.tm_mon
-# day = s.tm_mday
-# #end date is today
-#
-# startdate = str(year-1) + '-%02d' % (mon) + '-%02d' % (day)
-# enddate = str(year) + '-%02d' % (mon) + '-%02d' % (day)
-# result = neteaseconn.getDailyData(code=str("600000"), date1=startdate, date2=enddate)
-# for item in result:
-#     print item
-
-# startdate = str(year-2) + '-%02d' % (mon) + '-%02d' % (day)
-# enddate = str(year-1) + '-%02d' % (mon) + '-%02d' % (day)
-# result = sohuconn.getDailyData(code=str("600000"), date1=startdate, date2=enddate)
-# for item in result:
-#     print item
-
-# c = Collection()
-# c.getData(code="600000", start_date="2016-01-01", end_date="2018-12-31")
