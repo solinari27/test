4925a18d84dd69ebf7119b93e1a2a9cfe7cd1803
solinari <solinari27@gmail.com>
2019-02-08 16:39:37 +0800

    misc

--- torch_learning/PolynomialRegression.py
+++ torch_learning/PolynomialRegression.py
@@ -1,5 +1,5 @@
 #!usr/bin/env python
-#-*- coding:utf-8 _*-
+# -*- coding:utf-8 _*-
 """
 @author: solinari
 @file: PolynomialRegression.py
@@ -13,17 +13,22 @@ import torch.nn.functional as F
 
 POLY_DEGREE = 3
 
+
 def make_features(x):
     """Builds features i.e. a matrix with columns [x, x^2, x^3, x^4]."""
     x = x.unsqueeze(1)
-    return torch.cat([x ** i for i in range(1, POLY_DEGREE+1)], 1)  #torch.cat 实现tensor拼接
+    # torch.cat 实现tensor拼接
+    return torch.cat([x ** i for i in range(1, POLY_DEGREE + 1)], 1)
+
 
 W_target = torch.randn(POLY_DEGREE, 1)
 b_target = torch.randn(1)
 
+
 def f(x):
     """Approximated function."""
     return x.mm(W_target) + b_target.item()
+
 
 def get_batch(batch_size=32):
     """Builds a batch i.e. (x, f(x)) pair."""
@@ -31,6 +36,7 @@ def get_batch(batch_size=32):
     x = make_features(random)
     y = f(x)
     return x, y
+
 
 # Define model
 fc = torch.nn.Linear(W_target.size(0), 1)
@@ -68,6 +74,7 @@ def poly_desc(W, b):
         result += '{:+.2f} x^{} '.format(w, len(W) - i)
     result += '{:+.2f}'.format(b[0])
     return result
+
 
 print('Loss: {:.6f} after {} batches'.format(loss, batch_idx))
 print('==> Learned function:\t' + poly_desc(fc.weight.view(-1), fc.bias))
