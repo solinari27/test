21910687eb62dded334c8a38474176b527004282
solinari27 <solinari27@gmail.com>
2019-02-28 19:43:26 +0800

    data split

--- datamanager/pytorch/sklearn_lineregression.py
+++ datamanager/pytorch/sklearn_lineregression.py
@@ -1,12 +1,14 @@
 #!usr/bin/env python
-#-*- coding:utf-8 _*-
+# -*- coding:utf-8 _*-
 """
 @author: solinari
 @file: sklearn_lineregression.py
 @time: 2019/02/24
 """
-from sklearn import linear_model        #表示，可以调用sklearn中的linear_model模块进行线性回归。
+from sklearn import linear_model  # 表示，可以调用sklearn中的linear_model模块进行线性回归。
 import numpy as np
+from sklearn.ensemble import IsolationForest
+
 
 def get_batch(dataset):
     size = len(dataset)
@@ -17,15 +19,32 @@ def get_batch(dataset):
         y_list.append([dataset[i]['TCLOSE']])
     return x_rand, y_list
 
+
 def valid(dataset, model):
     X, y = dataset[0], dataset[1]
+    clf = IsolationForest(behaviour='new', max_samples=100,
+                          n_jobs=-1, verbose=2, contamination=0.01)
+
     diff = model.predict(X) - y
     _excps = []
     for _d in diff:
-        if _d > 0.1:
-            _excps.append([_d])
-            # print _d
-    print _excps
+        _excps.append(_d)
+
+    data_array = []
+    for _i in range(0, len(X)):
+        _x = X[_i][0]
+        _y = y[_i][0]
+        data_array.append([_x, _y])
+    data_array = np.array(data_array)
+    clf.fit(data_array)
+    pred = clf.predict(data_array)
+
+    index_list = []
+    for _i in range(0, len(pred)):
+        if pred[_i] == -1:
+            index_list.append([_i, _excps[_i]])
+
+    return index_list
 
 
 def do_regression(dataset, **kwargs):
@@ -35,6 +54,10 @@ def do_regression(dataset, **kwargs):
 
     w = model.coef_[0][0]
     b = model.intercept_[0]
-    valid([X, y], model)
+    index_list = valid([X, y], model)
+    while index_list != []:
+        pass
+        print index_list
+        #do split
 
     return w, b
