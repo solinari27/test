f48f4ec951af7068711be9be0b773e354022c720
solinari <solinari27@gmail.com>
2019-06-20 18:35:04 +0800

    test
    5.2
    
    bug fix:
    fix liangyee013 userkey
    Signed-off-by: solinari <solinari27@gmail.com>
    
    bug fix
    
    fix userid twice logging problem
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    xxx
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    xxx
    
    xxx
    
    xxx
    
    torch
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    xxx
    
    .gitignore is now working
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    try html spider
    
    bug fix
    
    xxx
    
    xxx
    
    x
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxxxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    login eDrive
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    xxx
    
    xxx
    
    x
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    login
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    xxx
    
    xxx
    
    request
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    upload
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    xxx
    
    xxx
    
    xxx
    
    upload
    
    xxx
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    xxx
    
    xxx
    
    xxx
    
    xxx
    
    xxx
    
    xxxx
    
    Signed-off-by: solinari <deeper1@163.com>
    
    x
    
    Signed-off-by: solinari <deeper1@163.com>
    
    sohu
    
    Signed-off-by: solinari <deeper1@163.com>
    
    netease
    
    Signed-off-by: solinari <deeper1@163.com>
    
    xxx
    
    xxx
    
    new data file module
    
    new data file module
    
    new data file module
    
    new data file module
    
    new data file module
    
    new data file module
    
    new data file module
    
    new data file module
    
    netease crawler bugfix
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    netease crawler bugfix
    
    Signed-off-by: solinari <solinari27@gmail.com>
    
    change
    
    change1
    
    bug to tensor
    
    perfect to get tensor format
    
    perfect to get tensor format
    
    perfect to get tensor format
    
    perfect to get tensor format
    
    bugfix for utf-8
    Signed-off-by: solinari <solinari27@gmail.com>
    
    torch less 1
    
    torch less 2
    
    format data from netease
    Signed-off-by: solinari <solinari27@gmail.com>
    
    format data from netease
    Signed-off-by: solinari <solinari27@gmail.com>
    
    bugfix for rename torch
    Signed-off-by: solinari <solinari27@gmail.com>
    
    bugfix for rename torch
    Signed-off-by: solinari <solinari27@gmail.com>
    
    bugfix for debugging collections
    
    use plotly to show data
    
    plotly show
    
    clear some code
    
    bugfix import
    
    plotly show dataset
    
    misc
    
    test regression dataset
    
    test regression dataset

--- datamanager/torch/testregression.py
+++ datamanager/torch/testregression.py
@@ -20,6 +20,10 @@ def make_features(x):
     return torch.cat([x ** i for i in range(1, POLY_DEGREE + 1)], 1)
 
 
+W_target = torch.randn(POLY_DEGREE, 1)
+b_target = torch.randn(1)
+
+
 def f(x):
     """Approximated function."""
     return x.mm(W_target) + b_target.item()
@@ -33,49 +37,44 @@ def get_batch(batch_size=32):
     return x, y
 
 
+# Define model
+fc = torch.nn.Linear(W_target.size(0), 1)
+
 ###################训练#########################
 
 
-def iter_batch():
+for batch_idx in count(1):
+    # Get data
+    batch_x, batch_y = get_batch()
 
-    W_target = torch.randn(POLY_DEGREE, 1)
-    b_target = torch.randn(1)
-    # Define model
-    fc = torch.nn.Linear(W_target.size(0), 1)
+    # Reset gradients
+    fc.zero_grad()
 
-    for batch_idx in count(1):
-        # Get data
-        batch_x, batch_y = get_batch()
+    # Forward pass
+    output = F.smooth_l1_loss(fc(batch_x), batch_y)
+    loss = output.item()
 
-        # Reset gradients
-        fc.zero_grad()
+    # Backward pass
+    output.backward()
 
-        # Forward pass
-        output = F.smooth_l1_loss(fc(batch_x), batch_y)
-        loss = output.item()
+    # Apply gradients
+    for param in fc.parameters():
+        param.data.add_(-0.1 * param.grad.data)
 
-        # Backward pass
-        output.backward()
+    # Stop criterion
+    if loss < 1e-3:
+        break
 
-        # Apply gradients
-        for param in fc.parameters():
-            param.data.add_(-0.1 * param.grad.data)
 
-        # Stop criterion
-        if loss < 1e-3:
-            break
+def poly_desc(W, b):
+    """Creates a string description of a polynomial."""
+    result = 'y = '
+    for i, w in enumerate(W):
+        result += '{:+.2f} x^{} '.format(w, len(W) - i)
+    result += '{:+.2f}'.format(b[0])
+    return result
 
-iter_batch()
 
-# def poly_desc(W, b):
-#     """Creates a string description of a polynomial."""
-#     result = 'y = '
-#     for i, w in enumerate(W):
-#         result += '{:+.2f} x^{} '.format(w, len(W) - i)
-#     result += '{:+.2f}'.format(b[0])
-#     return result
-#
-#
-# print('Loss: {:.6f} after {} batches'.format(loss, batch_idx))
-# print('==> Learned function:\t' + poly_desc(fc.weight.view(-1), fc.bias))
-# print('==> Actual function:\t' + poly_desc(W_target.view(-1), b_target))
+print('Loss: {:.6f} after {} batches'.format(loss, batch_idx))
+print('==> Learned function:\t' + poly_desc(fc.weight.view(-1), fc.bias))
+print('==> Actual function:\t' + poly_desc(W_target.view(-1), b_target))
