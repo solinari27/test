067d06ab43cf649048addd64483e4f76869ccc4c
solinari <deeper1@163.com>
2019-07-23 19:04:20 +0800

    keras good!

--- kera_sample/lstm_test.py
+++ kera_sample/lstm_test.py
@@ -11,8 +11,20 @@ from keras.layers import Dense, Dropout
 from keras.layers import Embedding
 from keras.layers import LSTM
 from keras.datasets import cifar10
+from keras.datasets import boston_housing
+from keras.datasets import imdb
 
-(x_train, y_train), (x_test, y_test) = cifar10.load_data()
+(x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
+                                                      num_words=None,
+                                                      skip_top=0,
+                                                      maxlen=None,
+                                                      seed=113,
+                                                      start_char=1,
+                                                      oov_char=2,
+                                                      index_from=3)
+
+# (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+# (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 max_features = 1024
 
