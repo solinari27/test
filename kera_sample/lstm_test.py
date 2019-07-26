0f535c02a1631b38f818d76e94b28af370ab1ef5
solinari <solinari27@gmail.com>
2019-07-26 21:57:31 +0800

    pytorch learning

--- kera_sample/lstm_test.py
+++ kera_sample/lstm_test.py
@@ -10,20 +10,21 @@ from keras.models import Sequential
 from keras.layers import Dense, Dropout
 from keras.layers import Embedding
 from keras.layers import LSTM
-from keras.datasets import cifar10
+# from keras.datasets import cifar10
 from keras.datasets import boston_housing
-from keras.datasets import imdb
+# from keras.datasets import imdb
 
-(x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
-                                                      num_words=None,
-                                                      skip_top=0,
-                                                      maxlen=None,
-                                                      seed=113,
-                                                      start_char=1,
-                                                      oov_char=2,
-                                                      index_from=3)
+# (x_train, y_train), (x_test, y_test) = imdb.load_data(path="imdb.npz",
+#                                                       num_words=None,
+#                                                       skip_top=0,
+#                                                       maxlen=None,
+#                                                       seed=113,
+#                                                       start_char=1,
+#                                                       oov_char=2,
+#                                                       index_from=3)
 
-# (x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+(x_train, y_train), (x_test, y_test) = boston_housing.load_data()
+
 # (x_train, y_train), (x_test, y_test) = cifar10.load_data()
 
 max_features = 1024
@@ -38,5 +39,5 @@ model.compile(loss='binary_crossentropy',
               optimizer='rmsprop',
               metrics=['accuracy'])
 
-model.fit(x_train, y_train, batch_size=16, epochs=10)
+model.fit(x_train, y_train, batch_size=16, epochs=100)
 score = model.evaluate(x_test, y_test, batch_size=16)
