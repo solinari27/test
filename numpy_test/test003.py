e9f2ae1e584f65df31aa572166e372374c7602e5
solinari <deeper1@163.com>
2019-07-28 22:02:01 +0800

    keras good!

--- numpy_test/test003.py
+++ numpy_test/test003.py
@@ -76,3 +76,7 @@ b = np.where(a < 50)
 c = np.where(a >= 50)[0]
 print(b) # >>>(array([0, 1, 2, 3, 4]),)
 print(c) # >>>[5 6 7 8 9]
+
+i=5
+i+=1
+print (i)
---
+++ numpy_test/test004.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2019/7/28 21:53
+# @Author  : Solinari
+# @Site    : 
+# @File    : test004.py
+# @Software: PyCharm
+
+animals = {'cat', 'dog'}
+print('cat' in animals)   # Check if an element is in a set; prints "True"
+print('fish' in animals)  # prints "False"
+animals.add('fish')       # Add an element to a set
+print('fish' in animals)  # Prints "True"
+print(len(animals))       # Number of elements in a set; prints "3"
+animals.add('cat')        # Adding an element that is already in the set does nothing
+print(len(animals))       # Prints "3"
+animals.remove('cat')     # Remove an element from a set
+print(len(animals))       # Prints "2"
+
+for item in animals:
+    print (item)
+
+
+import numpy as np
+d = np.eye(4)         # Create a 2x2 identity matrix
+print(d)
+
+
+print ("=================================================")
+
+x = np.array([[1,2],[3,4]], dtype=np.float64)
+y = np.array([[5,6],[7,8]], dtype=np.float64)
+
+# Elementwise sum; both produce the array
+# [[ 6.0  8.0]
+#  [10.0 12.0]]
+print(x + y)
+print(np.add(x, y))
+
+# Elementwise difference; both produce the array
+# [[-4.0 -4.0]
+#  [-4.0 -4.0]]
+print(x - y)
+print(np.subtract(x, y))
+
+# Elementwise product; both produce the array
+# [[ 5.0 12.0]
+#  [21.0 32.0]]
+print(x * y)
+print(np.multiply(x, y))
+
+# Elementwise division; both produce the array
+# [[ 0.2         0.33333333]
+#  [ 0.42857143  0.5       ]]
+print(x / y)
+print(np.divide(x, y))
+
+# Elementwise square root; produces the array
+# [[ 1.          1.41421356]
+#  [ 1.73205081  2.        ]]
+print(np.sqrt(x))
