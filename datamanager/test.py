6e0d1e1556321efc13ea4085d8cbb0bbf0e0e340
solinari <solinari27@gmail.com>
2019-09-05 22:39:10 +0800

    bugfix

--- datamanager/test.py
+++ datamanager/test.py
@@ -6,6 +6,9 @@
 @file: test.py
 @time: 2019/02/10
 """
+from pytorch.sklearn_lineregression import do_regression
+from collection import collection
+from collection import data_show
 import sys
 import time
 
@@ -21,16 +24,11 @@ sys.path.append('/mnt/d/workspace/test')
 # del(paths[len(paths)-1])
 # _path = os.path.join(paths)f
 # print (_path)
-from collection import data_show
-from collection import collection
-from pytorch.sklearn_lineregression import do_regression
 
 
 c = collection.Collection()
 
 for result in c.getData(code="600000", start_date="2010-01-01", end_date="2018-12-31"):
-    print (result)
-
     ret = do_regression(result, epochs=10000, thres=10,
                         DBSCAN_eps=3, DBSCAN_minsamples=4)
     print (ret)
@@ -39,7 +37,7 @@ for result in c.getData(code="600000", start_date="2010-01-01", end_date="2018-1
     #     b = item[1]
     #     dataset = result[item[2]: item[3]]
     #     print (dataset)
-        #show = data_show.Plt()
-        #show.load_data(data=dataset)
-        #show.plot(w=w, b=b)
-        #time.sleep(1)
+    #show = data_show.Plt()
+    # show.load_data(data=dataset)
+    #show.plot(w=w, b=b)
+    # time.sleep(1)
