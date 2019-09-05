1d3dfe516332a098451ac652278154e4c8c506b3
solinari27 <solinari27@gmail.com>
2019-09-05 09:39:37 +0800

    bugfix

--- datamanager/test.py
+++ datamanager/test.py
@@ -29,6 +29,8 @@ from pytorch.sklearn_lineregression import do_regression
 c = collection.Collection()
 
 for result in c.getData(code="600000", start_date="2010-01-01", end_date="2018-12-31"):
+    print (result)
+
     ret = do_regression(result, epochs=10000, thres=10,
                         DBSCAN_eps=3, DBSCAN_minsamples=4)
     for item in ret:
