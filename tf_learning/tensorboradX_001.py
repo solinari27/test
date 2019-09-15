3b7c77a458cf13686b708c843ff97079509cd8f5
solinari <solinari27@gmail.com>
2019-09-15 09:09:41 +0800

    matploblib show in tensorboardX

--- tf_learning/tensorboradX_001.py
+++ tf_learning/tensorboradX_001.py
@@ -6,62 +6,50 @@
 @time: 2018/10/09
 """
 
+# matplotlib with tensorboardX
+# tensorboardX dir in c:\Users\solinari\.tensorboard\data
+
+import torch
+from torch.autograd import Variable
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+plt.switch_backend('agg')
+
 from tensorboardX import SummaryWriter
-import cv2 as cv
 
-# writer = SummaryWriter('C:\Users\solinari\.tensorboard\data')
+
+# fake data
+x = torch.linspace(-5, 5, 200)
+x = Variable(x)
+x_np = x.data.numpy()
+
+y_relu = F.relu(x).data.numpy()
+y_sigmoid = F.sigmoid(x).data.numpy()
+y_tanh = F.tanh(x).data.numpy()
+y_softplus = F.softplus(x).data.numpy()
+# y_softmax = F.softmax(x)
+
+fig = plt.figure(1, figsize=(8, 6))
+plt.subplot(221)
+plt.plot(x_np, y_relu, c='red', label='relu')
+plt.ylim((-1, 5))
+plt.legend(loc='best')
+
+plt.subplot(222)
+plt.plot(x_np, y_sigmoid, c='red', label='sigmoid')
+plt.ylim((-0.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(223)
+plt.plot(x_np, y_tanh, c='red', label='tanh')
+plt.ylim((-1.2, 1.2))
+plt.legend(loc='best')
+
+plt.subplot(224)
+plt.plot(x_np, y_softplus, c='red', label='softplus')
+plt.ylim((-0.2, 6))
+plt.legend(loc='best')
+
 writer = SummaryWriter('/mnt/c/Users/solinari/.tensorboard/data')
-writer.add_image('countdown',
-                 cv.imread('data/image/jpg1.jiff'.format(1)))
-                 
-# for i in range(1, 6):
-#     writer.add_image('countdown',
-#                      cv.cvtColor(cv.imread('{}.jpg'.format(i)),
-#                                  cv.COLOR_BGR2RGB),
-#                      global_step=i,
-#                      dataformats='HWC')
-
-# import numpy as np
-# from tensorboardX import SummaryWriter
-# import tensorflow as tf
-
-# writer = SummaryWriter(log_dir='C:\Users\solinari\.tensorboard\data')
-# file = open('data/image/logo.png', 'rb')
-# data = file.read()
-# image = tf.image.decode_png(data, channels=4)
-# with tf.Session() as sess:
-#     # print type(sess.run(image))
-#     writer.add_image(tag='test', img_tensor=sess.run(image))
-
-# for epoch in range(100):
-
-#     writer.add_scalar('scalar/test', np.random.rand(), epoch)
-#     writer.add_scalars('scalar/scalars_test',
-#                        {'xsinx': epoch * np.sin(epoch), 'xcosx': epoch * np.cos(epoch)}, epoch)
-
+writer.add_figure(tag='activation_function', figure=fig)
 writer.close()
-
-
-# import tensorflow as tf
-
-# # 获取图片数据
-# file = open('data/image/logo.png', 'rb')
-# data = file.read()
-# file.close()
-
-# # 图片处理
-# image = tf.image.decode_png(data, channels=1)
-# image = tf.expand_dims(image, 0)
-
-# # 添加到日志中
-# sess = tf.Session()
-# writer = tf.summary.FileWriter('C:\Users\solinari\.tensorboard\data')
-# summary_op = tf.summary.image("image1", image)
-
-# # 运行并写入日志
-# summary = sess.run(summary_op)
-# writer.add_summary(summary)
-
-# # 关闭
-# writer.close()
-# sess.close()
