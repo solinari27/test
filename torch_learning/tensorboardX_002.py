3b6dfa3fad4d97bdbe365038ff61ac8434773442
solinari <solinari27@gmail.com>
2019-09-21 21:53:56 +0800

    bugfix

--- torch_learning/tensorboardX_002.py
+++ torch_learning/tensorboardX_002.py
@@ -1,4 +1,10 @@
 #!/usr/bin/env python
+# coding: utf-8
+
+# In[9]:
+
+
+#!/usr/bin/env python
 # -*- encoding: utf-8 -*-
 '''
 @File    :   tensorboardX_002.py
@@ -9,10 +15,47 @@
 '''
 
 # here put the import lib
-import matplotlib.pyplot as plt
-import seaborn as sns
+# %matplotlib inline
 import numpy as np
+import torch
+import matplotlib.pyplot as plt
+from tensorboardX import SummaryWriter
+from torch.autograd import Variable
+from uuid import uuid4
 
-sns.set_style("whitegrid")
-# plt.plot(np.arange(10))
-# plt.show()
+# switch matplotlib into non-interactive backennd
+# dont show img on screen
+plt.switch_backend('svg') # or 'svg' agg for png image svg for svg image
+
+# fake data
+x = torch.linspace(-5, 5, 200)
+x = Variable(x)
+x_np = x.data.numpy()
+
+# y_relu = F.relu(x).data.numpy()
+# y_sigmoid = F.sigmoid(x).data.numpy()
+y_tanh = torch.tanh(x).data.numpy()
+# y_softplus = F.softplus(x).data.numpy()
+# y_softmax = F.softmax(x)
+
+# fig = plt.figure(1, figsize=(8, 6))
+fig = plt.figure()
+plt.plot(x_np, y_tanh, c='red', label='relu')
+plt.ylim((-1, 5))
+plt.legend(loc='best')
+
+# writer = SummaryWriter('/mnt/c/Users/solinari/.tensorboard/img')
+# writer.add_figure(tag='activation_function_' + str(uuid4()), figure=fig)
+# writer.close()
+
+try:    
+    get_ipython().system('jupyter nbconvert --to python tensorboardX_002.ipynb')
+except:
+    pass
+
+
+# In[ ]:
+
+
+
+
