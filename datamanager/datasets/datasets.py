7d7ef2156aa50ecc8e0b41af1ce6f5ab1e7a9684
solinari <solinari27@gmail.com>
2019-10-25 20:41:12 +0800

    debug

--- datamanager/datasets/datasets.py
+++ datamanager/datasets/datasets.py
@@ -175,15 +175,17 @@ def clustering(rawdata, datas, extreme_points):
     ax.xaxis_date()
     plt.setp(plt.gca().get_xticklabels(), rotation=30)
     for _slice in _slices:
-        datet1 = datetime.datetime.strptime(rawdata[extreme_points[_slice[0]]]['DATE'], '%Y-%m-%d')
+        datet1 = datetime.datetime.strptime(rawdata[_slice[0]]['DATE'], '%Y-%m-%d')
         df1 = mpd.date2num(datet1)
         plt.vlines(df1, low, top, colors="c", linestyles="dashed")
+
 
     
     for _item in extreme_points:
         date2 = datetime.datetime.strptime(rawdata[_item]['DATE'], '%Y-%m-%d')
         df2 = mpd.date2num(date2)
         plt.scatter(df2, datas[_item], color='yellow', edgecolors='black')
+
 
     plt.show()
 
